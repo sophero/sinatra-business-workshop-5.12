af7613a7a2a7ad06d8e031df14c74b2fa7b6d6ba
Joshua Neidich <Joshua@reyoumassage.com>
2017-05-12 15:02:53 -0400

    highlighted active class

--- app.rb
+++ app.rb
@@ -1,17 +1,21 @@
 require 'sinatra'
 
 get '/' do
+	@class_1="active"
 	erb :home
 end
 
 get '/about' do
+	@class_2="active"
 	erb :about
 end
 
 get '/activities' do
+	@class_3="active"
 	erb :activities
 end
 
 get '/contact' do
+	@class_4="active"
 	erb :contact
 end
