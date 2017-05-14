ebb34530931bd8ab788c50ae84f2fe6e63ac7009
Sophia Di <backwardsxmarathon@gmail.com>
2017-05-14 18:00:40 -0400

    Adds nav tiles to bottom of carousel; also adds auto-scrolling which stops when user clicks to navigate between tiles.

--- public/scripts/home-carousel.js
+++ public/scripts/home-carousel.js
@@ -5,40 +5,83 @@ var kayak = new Image("kayak.jpg");
 var homeCarousel = new Carousel([hike, bike, kayak]);
 
 function Image(filename) {
-	this.url = "/assets/images/home-carousel/" + filename;
 	this.filename = filename;
+	var imageDir = "/assets/images/home-carousel/";
+	this.url = imageDir + filename;
 }
 
 function Carousel(imageArray) {
 	var images = imageArray;
 	var curImgIndex = 0;
+	var userClick = false;
 
 	var activeImage = document.getElementsByClassName('home-carousel__active-image')[0];
 	var leftArrow = document.getElementsByClassName('home-carousel__arrow-left')[0];
 	var rightArrow = document.getElementsByClassName('home-carousel__arrow-right')[0];
+	var tileNav = document.getElementsByClassName('home-carousel__tile-nav')[0];
 
-	leftArrow.addEventListener("click", prevImage);
-	rightArrow.addEventListener("click", nextImage);
+	leftArrow.addEventListener("click", function() {
+		userClick = true;
+		prevImage();
+	});
+	rightArrow.addEventListener("click", function() {
+		userClick = true;
+		nextImage();
+	});
 
-	displayImg();
+	displayNav();
+	var navTiles = document.getElementsByClassName('home-carousel__nav-tile');
+
+	setActiveTile(0);
+	autoScroll();
 
 	function nextImage() {
-		curImgIndex += 1;
-		if (curImgIndex === images.length) {
-			curImgIndex = 0;
+		if (curImgIndex === images.length - 1) {
+			var index = 0;
+		} else {
+			var index = curImgIndex + 1;
 		}
-		displayImg();
+		setActiveTile(index);
 	}
 
 	function prevImage() {
-		curImgIndex -= 1;
-		if (curImgIndex < 0) {
-			curImgIndex = images.length - 1;
+		if (curImgIndex === 0) {
+			var index = images.length - 1;
+		} else {
+			var index = curImgIndex - 1;
 		}
-		displayImg();
+		setActiveTile(index);
 	}
 
 	function displayImg() {
 		activeImage.style.backgroundImage = "url('" + images[curImgIndex].url + "')";
+	}
+
+	function setActiveTile(imageIndex) {
+		navTiles[curImgIndex].classList.remove("home-carousel__nav-tile--active");
+		navTiles[imageIndex].classList.add("home-carousel__nav-tile--active");
+		curImgIndex = imageIndex;
+		displayImg();
+	}
+
+	function displayNav() {
+		for (let k = 0; k < images.length; k++) {
+			var tileDiv = document.createElement("div");
+			tileDiv.classList.add("home-carousel__nav-tile");
+			tileDiv.addEventListener("click", function() {
+				userClick = true;
+				setActiveTile(k);
+			});
+			tileNav.append(tileDiv);
+		}
+	}
+
+	function autoScroll() {
+		setTimeout(function() {
+			if (userClick === false) {
+				nextImage();
+				autoScroll();
+			}
+		}, 5000);
 	}
 }
