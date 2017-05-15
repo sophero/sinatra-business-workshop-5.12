d5bd619fa9ccfb75f07fe6768c31338d1b1ab4a6
Sophia Di <backwardsxmarathon@gmail.com>
2017-05-15 11:10:03 -0400

    Menu toggles on window resize.

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,24 +1,33 @@
 var hamburgerIcon=document.getElementsByClassName('hamburger')[0];
-var navItem=document.getElementsByClassName('nav-item');
+var navItem=document.getElementsByClassName('nav-item')
 
-hamburgerIcon.addEventListener("click", showMenu);
+hamburgerIcon.addEventListener("click", toggleMenu);
 
-// function toggleMenu() {
-// 	if (navItem[0].style.display === "block") {
-// 		hideMenu();
-// 	} else {
-// 		showMenu();
-// 	}
-// }
+function toggleMenu() {
+	if (navItem[0].style.display === "block") {
+		hideMenu();
+	} else {
+		showMenu();
+	}
+}
 
-// function hideMenu() {
-// 	for (var i=0; i<navItem.length; i++){
-// 	    navItem[i].style.display="none";
-// 	  }	
-// }
+function hideMenu() {
+	for (var i=0; i<navItem.length; i++){
+	    navItem[i].style.display="none";
+	}	
+}
 
 function showMenu(){
-  for (var i=0; i<navItem.length; i++){
-    navItem[i].style.display="block";
-  }
+	for (var i=0; i<navItem.length; i++){
+		navItem[i].style.display="block";
+	}
 }
+
+window.addEventListener("resize", function() {
+	var width = window.innerWidth;
+	if (width > 480) {
+		showMenu();
+	} else {
+		hideMenu();
+	}
+});
