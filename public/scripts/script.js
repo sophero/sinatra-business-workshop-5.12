eb85e8340f23b195a34c0712027dcfbe99ce0162
Sophia Di <backwardsxmarathon@gmail.com>
2017-05-15 10:59:36 -0400

    Prevents menu from being hidden.

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,21 +1,21 @@
 var hamburgerIcon=document.getElementsByClassName('hamburger')[0];
 var navItem=document.getElementsByClassName('nav-item');
 
-hamburgerIcon.addEventListener("click", toggleMenu);
+hamburgerIcon.addEventListener("click", showMenu);
 
-function toggleMenu() {
-	if (navItem[0].style.display === "block") {
-		hideMenu();
-	} else {
-		showMenu();
-	}
-}
+// function toggleMenu() {
+// 	if (navItem[0].style.display === "block") {
+// 		hideMenu();
+// 	} else {
+// 		showMenu();
+// 	}
+// }
 
-function hideMenu() {
-	for (var i=0; i<navItem.length; i++){
-	    navItem[i].style.display="none";
-	  }	
-}
+// function hideMenu() {
+// 	for (var i=0; i<navItem.length; i++){
+// 	    navItem[i].style.display="none";
+// 	  }	
+// }
 
 function showMenu(){
   for (var i=0; i<navItem.length; i++){
