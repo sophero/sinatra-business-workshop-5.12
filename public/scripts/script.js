06289d948787a2247b79f68a03a7b1bcb986af31
Sophia Di <backwardsxmarathon@gmail.com>
2017-05-15 10:56:31 -0400

    Hamburger button toggles menu. Closes #16.

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,7 +1,21 @@
 var hamburgerIcon=document.getElementsByClassName('hamburger')[0];
-var navItem=document.getElementsByClassName('nav-item')
+var navItem=document.getElementsByClassName('nav-item');
 
-hamburgerIcon.addEventListener("click", showMenu)
+hamburgerIcon.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+	if (navItem[0].style.display === "block") {
+		hideMenu();
+	} else {
+		showMenu();
+	}
+}
+
+function hideMenu() {
+	for (var i=0; i<navItem.length; i++){
+	    navItem[i].style.display="none";
+	  }	
+}
 
 function showMenu(){
   for (var i=0; i<navItem.length; i++){
